2b7de8d83867ad15b5b8633891ea15b0c6f89ffa
Crummorn <igorcaze@gmail.com>
2021-03-21 02:26:34 -0300

    Chore: Estilizado SearchInput da pagina CharactersList.

--- src/pages/CharactersList/index.tsx
+++ src/pages/CharactersList/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-import { Container, Header, Title, SubTitle, Content } from './styles';
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  InputContainer,
+  InputIcon,
+  InputText,
+} from './styles';
 
 const CharactersList: React.FC = () => (
   <Container>
@@ -11,6 +20,19 @@ const CharactersList: React.FC = () => (
     </Header>
 
     <Content>
+      <InputContainer>
+        <InputIcon name="search" />
+
+        <InputText
+          keyboardAppearance="dark"
+          placeholderTextColor="#959595"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Busque por um personagem"
+          returnKeyType="send"
+        />
+      </InputContainer>
+
       <Text>Characters List</Text>
     </Content>
   </Container>
--- src/pages/CharactersList/styles.ts
+++ src/pages/CharactersList/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/Feather';
 
 export const Container = styled.View`
   flex: 1;
@@ -38,4 +39,35 @@ export const Content = styled.View`
   flex: 1;
 
   margin: 0 20px;
+`;
+
+export const InputContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  position: relative;
+  top: -30px;
+
+  height: 60px;
+
+  padding: 0 15px;
+  margin-bottom: -30px;
+
+  background: #ffffff;
+  border-radius: 6px;
+`;
+
+export const InputText = styled.TextInput`
+  flex: 1;
+
+  color: #222222;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const InputIcon = styled(Icon)`
+  margin-right: 16px;
+
+  color: #333333;
+  font-size: 20px;
 `;
