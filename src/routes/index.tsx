5f55bccd949e0d0c13cef91b5727c966dad1bf7e
Crummorn <igorcaze@gmail.com>
2021-03-21 17:22:54 -0300

    Chore: Adicionado rota para CharacterOverView.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Welcome from '../pages/Welcome';
 import CharactersList from '../pages/CharactersList';
+import CharacterOverView from '../pages/CharacterOverView';
 
 const App = createStackNavigator();
 
@@ -15,6 +16,7 @@ const AppRoutes: React.FC = () => (
   >
     <App.Screen name="Welcome" component={Welcome} />
     <App.Screen name="CharactersList" component={CharactersList} />
+    <App.Screen name="CharacterOverView" component={CharacterOverView} />
   </App.Navigator>
 );
 
