24f478d212ae640a34328cb0962e18fcab68b1de
Crummorn <igorcaze@gmail.com>
2021-03-20 22:45:45 -0300

    Chore: Finalizado estrutura/estilização da pagina Welcome

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import logoImage from '../../assets/logo.png';
+import backgroundImage from '../../assets/background.png';
 
-const Welcome: React.FC = () => <Container />;
+import {
+  Container,
+  Title,
+  Description,
+  ButtonContainer,
+  ButtonText,
+  ButtonIcon,
+} from './styles';
+
+const Welcome: React.FC = () => (
+  <Container source={backgroundImage} resizeMode="cover">
+    <Image source={logoImage} />
+
+    <Title>
+      Seja {'\n'}
+      Bem-Vindo
+    </Title>
+
+    <Description>
+      Aqui você vai ficar por dentro de todos os personagens da serie americana
+      breaking bad.
+    </Description>
+
+    <ButtonContainer
+      onPress={() => {
+        console.log('Proseguir...');
+      }}
+    >
+      <ButtonText>Proseguir</ButtonText>
+
+      <ButtonIcon name="arrow-right" />
+    </ButtonContainer>
+  </Container>
+);
 
 export default Welcome;
--- src/pages/Welcome/styles.ts
+++ src/pages/Welcome/styles.ts
@@ -1,5 +1,57 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Feather';
 
-export const Container = styled.View``;
+export const Container = styled.ImageBackground`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 
-export const Content = styled.View``;
+  width: 100%;
+  height: 100%;
+
+  padding: 0 30px;
+`;
+
+export const Title = styled.Text`
+  margin-top: 60px;
+
+  color: #ffffff;
+  text-align: center;
+  font-size: 40px;
+  font-family: 'Poppins-SemiBold';
+`;
+
+export const Description = styled.Text`
+  color: #ffffff;
+  text-align: center;
+  font-size: 15px;
+  font-family: 'Poppins-SemiBold';
+`;
+
+export const ButtonContainer = styled(RectButton)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 60px;
+
+  margin: 150px 0 35px;
+
+  background: #6d6d6d;
+  border-radius: 6px;
+`;
+
+export const ButtonText = styled.Text`
+  margin-right: 10px;
+
+  color: #e1e1e6;
+  font-size: 15px;
+  font-family: 'Poppins-Medium';
+`;
+
+export const ButtonIcon = styled(Icon)`
+  color: #e1e1e6;
+  font-size: 20px;
+`;
