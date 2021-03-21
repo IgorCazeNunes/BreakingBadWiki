f2b7881a4d93e55e653317efa2b4a7c591efd7fe
Crummorn <igorcaze@gmail.com>
2021-03-21 01:50:32 -0300

    Chore: Estilizado Header da pagina CharactersList.

--- src/pages/CharactersList/index.tsx
+++ src/pages/CharactersList/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Text } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Header, Title, SubTitle, Content } from './styles';
 
-const CharactersList: React.FC = () => <Container />;
+const CharactersList: React.FC = () => (
+  <Container>
+    <Header>
+      <Title>Listagem</Title>
+      <SubTitle>62 Personagens</SubTitle>
+    </Header>
+
+    <Content>
+      <Text>Characters List</Text>
+    </Content>
+  </Container>
+);
 
 export default CharactersList;
--- src/pages/CharactersList/styles.ts
+++ src/pages/CharactersList/styles.ts
@@ -1,5 +1,41 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
 
-export const Content = styled.View``;
+  background: #c4c4c4;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  height: 125px;
+
+  padding: 0 20px;
+
+  background: #333333;
+`;
+
+export const Title = styled.Text`
+  color: #ffffff;
+  font-size: 20px;
+  font-family: 'Poppins-SemiBold';
+`;
+
+export const SubTitle = styled.Text`
+  color: #ffffff;
+  font-size: 14px;
+  font-family: 'Poppins-Regular';
+`;
+
+export const Content = styled.View`
+  flex: 1;
+
+  margin: 0 20px;
+`;
